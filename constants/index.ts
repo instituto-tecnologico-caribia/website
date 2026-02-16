c9c7579c3b49e5965d7a7a209d78ba16aab7ec0d
Brayhan De Aza <brayhandeaza@gmail.com>
2026-02-15 19:19:27 -0500

    ..

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 
 
-export const MOODLE_URL = "http://localhost:8080/login/"
+export const MOODLE_URL = "https://your.caribia.edu.do/my/"
 export const LINKEDIN_URL = "https://www.linkedin.com/company/caribia"
 export const TWITTER_URL = "https://x.com/caribia_itc"
 export const INSTAGRAM_URL = "https://www.instagram.com/caribia.edu.do/"
